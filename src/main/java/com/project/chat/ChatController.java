c8190d8eae3337ec9ab89baf6fc79e2a3b035285
sumilon <sumilon.4you@gmail.com>
2024-03-17 14:07:30 +0530

    first commit

---
+++ src/main/java/com/project/chat/ChatController.java
@@ -0,0 +1,15 @@
+package com.project.chat;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public String sendMessage(String message) {
+        return message;
+    }
+}
